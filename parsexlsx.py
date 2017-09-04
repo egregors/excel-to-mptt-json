c9d909d884668822bdbfc3a597d63d7dc2d6aa5e
Maksim Kapranov <max.lamak@gmail.com>
2017-09-04 18:35:18 +1000

    full dicts

--- parsexlsx.py
+++ parsexlsx.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 from openpyxl import load_workbook
 from functools import reduce
+import json
 
 wb = load_workbook(filename='1.xlsx')
 ws = wb.active
+super_dict = {}
 
 # replace all none cells with upper cell, by column
 for col in ws.iter_cols():
@@ -13,11 +16,17 @@ for col in ws.iter_cols():
         else:
             tmp = cell.value
 
-# read to check
-for row in ws.iter_rows(min_row=3, max_col=4, max_row=3):  # debug on one row
+# put everything in one dict
+for row in ws.iter_rows():  # full table or use min_row=3, max_col=4, max_row=10 to debug on few rows
+    #parse cell row2list
     c = []
     for cell in row:
         c.append(cell.value)
-    print(c)
-    d = reduce(lambda x, y: {y: x}, reversed(c + ['']))
-    print(d)
+    #list2dict
+    d = reduce(lambda x, y: {'title': y, 'children': [x]}, reversed(c), {})
+    #dict2superdicts
+    for key, value in d.items():
+        super_dict.setdefault(key, []).append(value)
+
+#print(super_dict)
+print(json.dumps(super_dict,sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
